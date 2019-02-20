2b04e0a3b8049f62af42d1f8d2fda6829e84c4c0
Chauhan Sanjay <sanjay.chauhan@nokia.com>
2019-02-20 22:49:43 +0530

    Update structure

---
+++ src/com/nokia/sanjay/impl/Tester.java
@@ -0,0 +1,36 @@
+package com.nokia.sanjay.impl;
+
+import com.nokia.sanjay.apis.RegexpMatchCalculator;
+import com.nokia.sanjay.exceptions.RegexpException;
+
+/**
+ * 
+ * @author <a href=
+ *         "https://www.youracclaim.com/badges/1cacfc10-a704-44ed-94b4-1cd7abbfd168/public_url"
+ *         style="color:red; font-size:16; "><em>Sanjay Chauhan</em></a>
+ */
+
+public class Tester {
+
+	public static void main(String[] args) {
+
+		RegexpMatchCalculator regexpCalculator = new RegexpMatchCalculatorImpl();
+
+		try {
+
+			regexpCalculator.findRegexpMatch("Source string here");
+
+		} catch (RegexpException e) {
+
+			System.out.println(e.getErrorMessage());
+			System.out.println(e.getErrorCode());
+
+		} catch (Throwable ex) {
+
+			System.out.println("Unexpected error occured! Reason : " + ex.getMessage());
+
+		}
+
+	}
+
+}
---
+++ src/com/nokia/sanjay/impl/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 
+ * @author <a href=
+ *         "https://www.youracclaim.com/badges/1cacfc10-a704-44ed-94b4-1cd7abbfd168/public_url"
+ *         style="color:red; font-size:16; "><em>Sanjay Chauhan</em></a>
+ */
+package com.nokia.sanjay.impl;
